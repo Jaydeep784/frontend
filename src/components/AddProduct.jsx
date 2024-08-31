a3951b30748ccb876fed9da4692a489da1fc63cd
Jaydeep Jadhav <91197991+Jaydeep784@users.noreply.github.com>
2024-08-31 15:15:03 +0530

    Update AddProduct.jsx

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -21,7 +21,7 @@ const AddProduct = () => {
     )
       return alert("Enter data in all fields");
     axios
-      .post("backend-six-inky-58.vercel.app/productDetails", productDetail)
+      .post("https://backend-1bqu.onrender.com//productDetails", productDetail)
       .then((res) => console.log(res))
       .catch((e) => console.log(e));
   };
